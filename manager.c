a9eff6109d2b381f19cdf347fcf70f1a1d66f805
DajeongLee <22100493@handong.ac.kr>
2022-04-17 19:51:17 +0900

    add menu of saving data

--- manager.c
+++ manager.c
@@ -9,6 +9,7 @@ int selectMenu(){
     printf("3. 수정\n");
     printf("4. 삭제\n");
     printf("5. 제품 검색\n");
+    printf("6. 파일 저장\n");
     printf("0. 종료\n\n");
     printf("=> 원하는 메뉴는? ");
     scanf("%d", &menu);
