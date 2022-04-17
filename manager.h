3d95f7f71619a9b9d04ce1a6e6319b89be3933ab
Dazunx <97433636+Dazunx@users.noreply.github.com>
2022-04-18 01:15:59 +0900

    Update manager.h
    LoadData parameter correction, saveData typo correction

--- manager.h
+++ manager.h
@@ -13,16 +13,16 @@ typedef struct
     int price;  // 판매가격
     int deli; // 배송방법 (1:새벽배송 /2:택배배송)  
 }Product;
-int loadData(Product *p[], int count); // 파일에서 데이터 불러오는 함수
-void savdData(Product *p[], int count); // 파일에 데이터 저장하는 함수
+
 int createProduct(Product *p); // 제품을 추가하는 함수
 int readProduct(Product p); // 하나의 제품 출력 함수
 int listProduct(Product *p[], int count); // 전체 등록된 제품 리스트 출력
 int updateProduct(Product *p); // 제품내용 수정할 함수
 int deleteProduct(Product *p[], int count); // 제품 삭제 함수
 void searchProduct(Product *p[], int count); // 제품이름 검색
+void saveData(Product *p[],int count); // 파일에 데이터 저장하는 함수
+int loadData(Product *p[]); // 파일에서 데이터 불러오는 함수
 int selectDataNo(Product *p[], int count);
 int selectMenu();
-void savdData(Product *p[],int count); // 파일에 데이터 저장하는 함수
-int loadData(Product *p[],int index); // 파일에서 데이터 불러오는 함수
+
 
